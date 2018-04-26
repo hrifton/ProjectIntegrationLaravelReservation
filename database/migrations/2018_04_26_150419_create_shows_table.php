82e999d0ced6ce68e8370e78bdcf9039de528d6f
Asbagui <asbagui.hicham@outlook.com>
2018-04-26 18:21:46 +0200

    creation ModelTable

---
+++ database/migrations/2018_04_26_150419_create_shows_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shows', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->nullable();
+            $table->string('titre')->nullable();
+            $table->string('poster_url')->nullable();
+            $table->integer('location_id')->references('id')->on('locations')->nullable();
+            $table->smallInteger('bookable')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shows');
+    }
+}
---
+++ database/migrations/2018_04_26_151546_create_representation_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepresentationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('representations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('show_id')->references('id')->on('shows')->nullable();
+            $table->datetime('when')->nullable();
+            $table->integer('location_id')-references('id')->on('localities')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('representations');
+    }
+}
